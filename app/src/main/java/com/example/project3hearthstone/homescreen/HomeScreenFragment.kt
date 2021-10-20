1f056f0ae38dc51ff48919e9e7b758547c89d5bf
greenstriker99 <imakftw@gmail.com>
2021-10-20 10:13:48 -0500

    setup project navigation, dependencies and home screen stuff

---
+++ app/src/main/java/com/example/project3hearthstone/homescreen/HomeScreenFragment.kt
@@ -0,0 +1,19 @@
+package com.example.project3hearthstone.homescreen
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.project3hearthstone.R
+
+class HomeScreenFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.home_screen_fragment, container, false)
+    }
+}
